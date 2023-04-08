67e29f68f1db2708b7cb3267709f9ac124e62f9e
Suhas Jujjavarapu <suhasj2003@suhass-mbp.umd.edu>
2023-04-08 18:29:04 -0400

    initialize spring boot + mongodb connection

---
+++ api/src/test/java/com/bitcamp2023/hobbyrecapi/HobbyrecapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitcamp2023.hobbyrecapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyrecapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- client/components/Icon.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Ionicons } from "@expo/vector-icons";
-import { IconT } from "../types";
-
-const Icon = ({ color, name, size, style }: IconT) => (
-  <Ionicons name={name} size={size} color={color} style={style} />
-);
-
-export default Icon;
