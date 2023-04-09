0e83480c5ee0863ddb5d46b2c8b5f948fbfa4aac
Suhas Jujjavarapu <suhasj2003@suhass-mbp.umd.edu>
2023-04-08 20:43:29 -0400

    Remove mongodb deps + Create user class functionality

--- api/src/main/java/com/bitcamp2023/hobbyrecapi/HobbyrecapiApplication.java
+++ api/src/main/java/com/bitcamp2023/hobbyrecapi/HobbyrecapiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HobbyrecapiApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(HobbyrecapiApplication.class, args);
 	}
-
 }
